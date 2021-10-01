121ab8a4c3a9fb49839c6631fe23c3d6bb4b00b4
Nels Bjork <nbjork1@cnm.edu>
2021-10-01 14:50:16 -0500

    add to github

--- Scripts.js
+++ Scripts.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-
+initcolorpicker()
 };
 function initcolorpicker () {
     let colorbox = document.getElementById("color-box");
@@ -8,14 +8,16 @@ function initcolorpicker () {
         green: document.getElementById("green"),
         blue: document.getElementById("blue")
     };
-    let colorPickers = document.getElementsByClassName("pickers");
+console.log(colorbox)
+
+    let colorPickers = document.getElementsByClassName("picker");
     SetColorPickerEventListeners(colorbox, rgb, colorPickers);
 }
 function SetColorPickerEventListeners(element, colors, pickerElements) {
     let pickerLen = pickerElements.length;
     for (let i = 0; i < pickerLen; i++){
         pickerElements[i].addEventListener('change', () => {
-            console.log("red value: ", rgb.red.value);
+
             let red = colors.red.value;
             let green = colors.green.value;
             let blue = colors.blue.value;
@@ -25,7 +27,8 @@ function SetColorPickerEventListeners(element, colors, pickerElements) {
     }
 
     function setElementBGColor(bgElement, red, green, blue) {
-         let rgbVal = [red, green, blue].join('.');
+         let rgbVal = [red, green, blue].join(',');
+         console.log(rgbVal)
          bgElement.style.backgroundColor = "rgb(" + rgbVal + ")";
     }
 
